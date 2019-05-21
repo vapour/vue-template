12b522f78ffe662dd46a67e8bc88abc5a678e4b5
杜伟坡 <dovapour@qq.com>
2019-05-21 09:53:57 +0800

    jsdoc

--- README.md
+++ README.md
@@ -82,33 +82,66 @@ this.$axios.get('/erp/common/shop', {
 以下是**api/todo.js**内容
 
 ```
+/**
+ * 待办事项ajax服务
+ * @module api/todo
+ *
+ */
 import axios from '@/plugins/axios'
 
 const url = '/todo'
 
 // 待办事项服务接口
 export default {
-  // 分页获取
-  list(params) {
-    return axios.get(url, { params })
+  /**
+   * 分页获取
+   * @param {object} param  请求参数
+   * @param {number} [param.page] 当前页数
+   * @param {number} param.pageSize 分页大小
+   * @return {Promise}
+   */
+  list(param) {
+    return axios.get(url, { param })
   },
-  // 新建
-  create(params) {
-    return axios.post(url, params)
+  /**
+   * 新建数据
+   * @param {object} param  请求参数
+   * @param {string} param.name 事项名称
+   * @param {string} param.content 详细描述
+   * @return {Promise}
+   */
+  create(param) {
+    return axios.post(url, param)
   },
-  // 获取详情
+  /**
+   * 获取详情
+   * @param {string} id
+   * @return {Promise}
+   */
   show(id) {
     return axios.get(url + '/' + id)
   },
-  // 更新
-  update(id, params) {
-    return axios.put(url + '/' + id, params)
+  /**
+   * 更新单个数据
+   * @param {string} id  数据id
+   * @param {object} param  更新数据对象
+   * @param {string} param.name 事项名称
+   * @param {string} param.content 详细描述
+   * @return {Promise}
+   */
+  update(id, param) {
+    return axios.put(url + '/' + id, param)
   },
-  // 删除
+  /**
+   * 删除单个数据
+   * @param {string} id
+   * @return {Promise}
+   */
   delete(id) {
     return axios.delete(url + '/' + id)
   }
 }
+
 ```
 
 ## 模拟请求
--- src/api/todo.js
+++ src/api/todo.js
@@ -11,34 +11,48 @@ const url = '/todo'
 export default {
   /**
    * 分页获取
-   * @property {object} params  请求参数
-   * @property {number} [params.page] 当前页数
-   * @property {number} params.pageSize 分页大小
+   * @param {object} param  请求参数
+   * @param {number} [param.page] 当前页数
+   * @param {number} param.pageSize 分页大小
    * @return {Promise}
    */
-  list(params) {
-    return axios.get(url, { params })
+  list(param) {
+    return axios.get(url, { param })
   },
   /**
    * 新建数据
-   * @property {object} params  请求参数
-   * @property {string} params.name 事项名称
-   * @property {string} params.content 详细描述
+   * @param {object} param  请求参数
+   * @param {string} param.name 事项名称
+   * @param {string} param.content 详细描述
    * @return {Promise}
-   *
    */
-  create(params) {
-    return axios.post(url, params)
+  create(param) {
+    return axios.post(url, param)
   },
-  // 获取详情
+  /**
+   * 获取详情
+   * @param {string} id
+   * @return {Promise}
+   */
   show(id) {
     return axios.get(url + '/' + id)
   },
-  // 更新
-  update(id, params) {
-    return axios.put(url + '/' + id, params)
+  /**
+   * 更新单个数据
+   * @param {string} id  数据id
+   * @param {object} param  更新数据对象
+   * @param {string} param.name 事项名称
+   * @param {string} param.content 详细描述
+   * @return {Promise}
+   */
+  update(id, param) {
+    return axios.put(url + '/' + id, param)
   },
-  // 删除
+  /**
+   * 删除单个数据
+   * @param {string} id
+   * @return {Promise}
+   */
   delete(id) {
     return axios.delete(url + '/' + id)
   }
