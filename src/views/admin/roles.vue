a63c71a8859cbb6dad0aacdcc62904eb122bd06c
杜伟坡 <dovapour@qq.com>
2019-06-27 15:19:15 +0800

    Update roles.vue

--- src/views/admin/roles.vue
+++ src/views/admin/roles.vue
@@ -12,21 +12,6 @@ export default {
   },
   mounted() {
     console.log(this.helper.toJson({ name: 'vapour', desc: 'pddd' }))
-    this.$axios.get('/erp/common/shop', {
-      globalLoading: false
-    }).then(({ data: res }) => {
-      console.log(res)
-    })
-
-    const fd = new FormData()
-    fd.append('id', 'MGP923432424')
-    this.$axios.post('/erp/order/data/globalSearch.json', fd).then(({ data: res }) => {
-      console.log(res)
-    })
-
-    this.$axios.post('/erp/order/data/globalSearch.json', { id: 'MGP923432424' }).then(({ data: res }) => {
-      console.log(res)
-    })
   }
 }
 </script>
