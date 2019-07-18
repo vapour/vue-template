362ffef04a0ace342ab312d0cac6896b82d4eb20
杜伟坡 <dovapour@qq.com>
2019-07-18 14:37:33 +0800

    win32 check cert
    window下检测是否已经安装证书

--- scripts/tls/util.js
+++ scripts/tls/util.js
@@ -1,15 +1,21 @@
 const fs = require('fs')
 const path = require('path')
 const forge = require('node-forge')
+const axios = require('axios')
 
 const config = require('../../vue.config.js')
 // 读取webpackDevServerPath
 require('webpack-dev-server/package.json')
 
+const caCertPath = path.join(__dirname, './cert/f2e.ca.crt')
+const caKeyPath = path.join(__dirname, './cert/f2e.ca.key.pem')
+
 
 const pki = forge.pki
 
 exports.createCA = function() {
+  
+  console.log('CA证书开始创建')
   const keys = pki.rsa.generateKeyPair(2046)
   const cert = pki.createCertificate()
   cert.publicKey = keys.publicKey
@@ -59,13 +65,29 @@ exports.createCA = function() {
   const certPem = pki.certificateToPem(cert)
   const keyPem = pki.privateKeyToPem(keys.privateKey)
 
-  fs.writeFileSync(path.join(__dirname, './cert/f2e.ca.crt'), certPem)
-  fs.writeFileSync(path.join(__dirname, './cert/f2e.ca.key.pem'), keyPem)
+  fs.writeFileSync(caCertPath, certPem)
+  fs.writeFileSync(caKeyPath, keyPem)
+  console.log('CA证书创建成功')
+}
+
+exports.initCA = async function () {
+  try {
+    // 如果证书已经存在，直接返回
+    fs.accessSync(caCertPath, fs.F_OK)
+    fs.accessSync(caKeyPath, fs.F_OK)
+    console.log('CA证书已经存在')
+    return
+  } catch (e) {
+  }
+  console.log('CA证书开始获取')
+  await getRemoteFile('https://raw.githubusercontent.com/vapour/vue-template/ssl/scripts/tls/cert/f2e.ca.crt', caCertPath)
+  await getRemoteFile('https://raw.githubusercontent.com/vapour/vue-template/ssl/scripts/tls/cert/f2e.ca.key.pem', caKeyPath)
+  console.log('CA证书获取完成')
 }
 
 let createFakeCertificateByDomain = exports.createFakeCertificateByDomain = function (domain) {
-  const caKeyPem = fs.readFileSync(path.join(__dirname, './cert/f2e.ca.key.pem'))
-  const caCertPem = fs.readFileSync(path.join(__dirname, './cert/f2e.ca.crt'))
+  const caKeyPem = fs.readFileSync(caKeyPath)
+  const caCertPem = fs.readFileSync(caCertPath)
   caCert = pki.certificateFromPem(caCertPem)
   caKey = pki.privateKeyFromPem(caKeyPem)
 
@@ -137,14 +159,6 @@ let createFakeCertificateByDomain = exports.createFakeCertificateByDomain = func
   }])
   cert.sign(caKey, forge.md.sha256.create())
 
-  /*
-  const certPem = pki.certificateToPem(cert)
-  const keyPem = pki.privateKeyToPem(keys.privateKey)
-
-  fs.writeFileSync(path.join(__dirname, './cert/' + domain + '.crt'), certPem)
-  fs.writeFileSync(path.join(__dirname, './cert/' + domain + '.key.pem'), keyPem)
-  */
-
   return {
     key: keys.privateKey,
     cert: cert
@@ -171,5 +185,20 @@ exports.updateWebpackCert = function () {
     const name = Object.keys(cache).find(key => key.includes('webpack-dev-server' + path.sep + 'package.json'))
     return path.dirname(cache[name].filename)
   }
+}
+
+// 读取远程文件
+function getRemoteFile(url, filePath) {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'get',
+      url,
+      responseType: 'stream'
+    }).then(response => {
+      let stream = fs.createWriteStream(filePath)
+      stream.on('close', resolve)
+      response.data.pipe(stream)
+    }, reject)
+  })
 }
 
